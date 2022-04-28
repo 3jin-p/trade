9ac1c1f7e87e453a6492fddf3f7fb3c893fbf6a1
sejin <3jin.pk@gmail.com>
2022-04-28 11:00:06 +0900

    test: concurrency test 불필요한 테스트 제거 및 수정

--- src/test/kotlin/com/mp/trade/integration/TradeConcurrencyTest.kt
+++ src/test/kotlin/com/mp/trade/integration/TradeConcurrencyTest.kt
@@ -3,18 +3,16 @@ package com.mp.trade.integration
 import com.mp.trade.domain.PayPoint
 import com.mp.trade.domain.Point
 import com.mp.trade.domain.Trade
-import com.mp.trade.dto.PayPointRequest
 import com.mp.trade.dto.TradeRequest
 import com.mp.trade.repo.PayPointRepository
 import com.mp.trade.repo.TradeRepository
-import com.mp.trade.service.PayPointService
 import com.mp.trade.service.TradeService
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.lang.RuntimeException
 import java.util.*
 import java.util.concurrent.Executors
 
@@ -22,8 +20,6 @@ import java.util.concurrent.Executors
 class TradeConcurrencyTest {
     @Autowired
     lateinit var tradeService: TradeService
-    @Autowired
-    lateinit var payPointService: PayPointService
     @Autowired
     lateinit var payPointRepository: PayPointRepository
     @Autowired
@@ -47,24 +43,11 @@ class TradeConcurrencyTest {
                 tradeService.openTrade(TradeRequest(payPointId, Trade.TradeType.DEPOSIT, Point(1000))) }
         }
 
-        // 이벤트까지 처리되는건 테스트안된다. 다른방법쓰기
+        Thread.sleep(3000) // 비동기 event 처리 대기
         val result = payPointRepository.findById(payPointId).orElseThrow { RuntimeException() }
-        assertThat(result.amount).isEqualTo(10000)
-    }
+        assertThat(result.amount).isEqualTo(Point(10000))
 
-    @Test
-    fun testDeposit2() {
-        val executorService = Executors.newFixedThreadPool(10)
-
-        for (i in 1 .. 10) {
-            // trade init Open Event 발생안시킬시 통과
-            val trade = tradeRepository.save(Trade(payPointId, Trade.TradeType.DEPOSIT, Point(1000)))
-
-            executorService.execute {
-                payPointService.deposit(PayPointRequest.DepositRequest(trade.id, payPointId, Point(1000))) }
-        }
-
-        val result = payPointRepository.findById(payPointId).orElseThrow { RuntimeException() }
-        assertThat(result.amount).isEqualTo(10000)
+        val trades = tradeRepository.findAll()
+        assertTrue(trades.stream().allMatch { t -> t.status == Trade.TradeStatus.SUCCESS })
     }
 }
