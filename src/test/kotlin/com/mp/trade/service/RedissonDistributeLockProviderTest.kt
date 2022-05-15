bf1a8d5f7d91bf86635bc6a477bead5b30e4dc86
sejinpark <3jin.pk@gmail.com>
2022-05-15 14:00:42 +0900

    feat: #3 redisson 분산락 provider 타임아웃 테스트

--- src/test/kotlin/com/mp/trade/service/RedissonDistributeLockProviderTest.kt
+++ src/test/kotlin/com/mp/trade/service/RedissonDistributeLockProviderTest.kt
@@ -3,13 +3,10 @@ package com.mp.trade.service
 import com.mp.trade.support.RedissonDistributeLockProvider
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.lang.IllegalArgumentException
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.Lock
 
 @SpringBootTest
 internal class RedissonDistributeLockProviderTest(
@@ -30,15 +27,41 @@ internal class RedissonDistributeLockProviderTest(
         //when
         val executors = Executors.newFixedThreadPool(10)
 
-        for (i in 1 .. 100) {
-           executors.execute { redissonDistributeLockProvider.lock(lockId, lockWaitSeconds, lockTtl) { number.plus() } }
+        for (i in 1 .. 10) {
+           executors.execute {
+               redissonDistributeLockProvider.lock(lockId, lockWaitSeconds, lockTtl) { number.plus() } }
         }
 
         executors.shutdown();
         executors.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 
         // then
-        assertThat(number.number).isEqualTo(100)
+        assertThat(number.number).isEqualTo(10)
+    }
+
+    @Test
+    fun testLockTimeOut() {
+        //given
+        val lockId = "lockId"
+        val lockWaitSeconds = 1L
+        val lockTtl = 100L
+
+        val number = Number(0)
+
+
+        //when
+        val executors = Executors.newFixedThreadPool(10)
+
+        for (i in 1 .. 10) {
+            executors.execute { redissonDistributeLockProvider.lock(lockId, lockWaitSeconds, lockTtl) {
+                number.sleepAndPlus(2000) } }
+        }
+
+        executors.shutdown();
+        executors.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+        // then
+        assertThat(number.number).isEqualTo(1)
     }
 
 
@@ -48,6 +71,11 @@ internal class RedissonDistributeLockProviderTest(
 
         fun plus() {
             this.number += 1
+        }
+
+        fun sleepAndPlus(ms: Long) {
+            Thread.sleep(ms)
+            plus()
         }
 
     }
